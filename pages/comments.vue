03a06013170c85cef51cf56847aa6fdeea571933
Frank <frankortizm849@gmail.com>
2021-02-08 15:35:24 -0600

    delay de 500 milisegundos base agregados a carga de comentarios

--- pages/comments.vue
+++ pages/comments.vue
@@ -1,12 +1,14 @@
 <template >
   <div class="comments_div_general">
-    <h1 :class="'dm_t1_'+darkmode">Sugerencias y opiniones</h1>
+    <h1 :class="'dm_t1_' + darkmode">Sugerencias y opiniones</h1>
     <div class="commetns_div_cont_insertions">
-      <h2 :class="'comments_titulo_form dm_t1_'+darkmode">Envianos tu experiencia</h2>
+      <h2 :class="'comments_titulo_form dm_t1_' + darkmode">
+        Envianos tu experiencia
+      </h2>
       <form id="comments_form" class="comments_form">
         <input
           id="name"
-          :class="'comments_nombre c_item_form dm_t3_'+darkmode"
+          :class="'comments_nombre c_item_form dm_t3_' + darkmode"
           type="text"
           required
           autocomplete="off"
@@ -14,7 +16,7 @@
           maxlength="20"
         />
         <textarea
-          :class="'comments_comentario c_item_form dm_t3_'+darkmode"
+          :class="'comments_comentario c_item_form dm_t3_' + darkmode"
           name="comentario"
           id="comentario"
           placeholder="Comentario"
@@ -26,15 +28,20 @@
           @click="sendComment"
           type="submit"
           id="bot"
-          :class="'commetns_boton_send c_item_form dm_border_'+darkmode"
+          :class="'commetns_boton_send c_item_form dm_border_' + darkmode"
         >
           Enviar
         </button>
       </form>
-      <div v-show="invalid" :class="'commetns_invalid error_'+lighterror+' dm_border_'+darkmode">
+      <div
+        v-show="invalid"
+        :class="
+          'commetns_invalid error_' + lighterror + ' dm_border_' + darkmode
+        "
+      >
         Favor ingrese datos validos
       </div>
-      <div v-show="sended" :class="'commetns_sended dm_border_'+darkmode">
+      <div v-show="sended" :class="'commetns_sended dm_border_' + darkmode">
         Enviado correctamente
       </div>
     </div>
@@ -42,14 +49,28 @@
     <div class="model1-div-cont-separator"><hr class="model1-separator" /></div>
 
     <div class="div_cont_allcomments">
-      <h2 :class="'comments_titulo_seccion_comentarios dm_t1_'+darkmode">Comentarios:</h2>
-      <div v-show="loading" class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+      <h2 :class="'comments_titulo_seccion_comentarios dm_t1_' + darkmode">
+        Comentarios:
+      </h2>
+      <div v-show="loading" class="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
       <commentIndividual
-        v-for="(coment, index) in parcialComentarios" :key="index"
+        v-for="(coment, index) in parcialComentarios"
+        :key="index"
         :nombre="comentarios[index].nombre"
         :comentario="comentarios[index].comentario"
       />
-      <div v-show="booleanshowmore" @click="moreComments" :class="'commetns_more dm_t3_'+darkmode">Mas comentarios</div>
+      <div
+        v-show="booleanshowmore"
+        @click="moreComments"
+        :class="'commetns_more dm_t3_' + darkmode"
+      >
+        Mas comentarios
+      </div>
     </div>
   </div>
 </template>
@@ -62,33 +83,34 @@ export default {
   components: {
     CommentIndividual,
   },
-  head(){
-      return{
-        title: 'Japanese Practice - Sugerencias',
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: 'Plataforma para practicar el idioma Japones con sus alfabetos de Hiragana y Katakana'
-          }
-        ],
-        htmlAttrs:{
-          lang: 'es',
-        }
-      }
-    },
+  head() {
+    return {
+      title: "Japanese Practice - Sugerencias",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Plataforma para practicar el idioma Japones con sus alfabetos de Hiragana y Katakana",
+        },
+      ],
+      htmlAttrs: {
+        lang: "es",
+      },
+    };
+  },
   data() {
     return {
-      comentarios:[],
-      parcialComentarios:[],
-      indexParcialComments:0,
+      comentarios: [],
+      parcialComentarios: [],
+      indexParcialComments: 0,
       nombre: "",
       comentario: "",
       invalid: false,
       sended: false,
       lighterror: false,
-      booleanshowmore:true,
-      loading:true,
+      booleanshowmore: true,
+      loading: true,
     };
   },
   computed: {
@@ -105,7 +127,7 @@ export default {
         nombre.length > 20 ||
         comentario.length > 250 ||
         nombre == "" ||
-        comentario == "" 
+        comentario == ""
       ) {
         docnombre.value = "";
         doccoment.value = "";
@@ -115,7 +137,7 @@ export default {
         this.lighterror = true;
         //this.lighterror = false;
         setTimeout(this.lighter, 300);
-        
+
         return;
       } else {
         let nombreClean = nombre.trim();
@@ -142,34 +164,35 @@ export default {
       docnombre.value = "";
       doccoment.value = "";
     },
-    lighter(){
+    lighter() {
       this.lighterror = false;
     },
-    async getCommetns(){
+    async getCommetns() {
       try {
-        const respuesta = await axios.get('https://japanese-backend.herokuapp.com/getcomments');
+        const respuesta = await axios.get(
+          "https://japanese-backend.herokuapp.com/getcomments"
+        );
         console.log(respuesta.data);
         this.comentarios = respuesta.data.reverse();
-        this.loading = false;
-        this.moreComments();
+        setTimeout( this.moreComments, 500);
+        console.log('no shit');
       } catch (error) {
         console.error(error);
         console.log(error);
       }
     },
-    moreComments(){
+    moreComments() {
+      this.loading = false;
       let limit = this.indexParcialComments;
-      
-        for (let i = this.indexParcialComments; i < limit+2; i++) {
-          if(this.indexParcialComments < this.comentarios.length){
-            this.parcialComentarios.push(this.comentarios[i]);
-            this.indexParcialComments++;
-          } else {
-            this.booleanshowmore = false;
-            console.log('no hay mas por mostrar desde adentro');
-          }
+      for (let i = this.indexParcialComments; i < limit + 2; i++) {
+        if (this.indexParcialComments < this.comentarios.length) {
+          this.parcialComentarios.push(this.comentarios[i]);
+          this.indexParcialComments++;
+        } else {
+          this.booleanshowmore = false;
+          console.log("no hay mas por mostrar desde adentro");
         }
-      
+      }
     },
   },
   mounted() {
